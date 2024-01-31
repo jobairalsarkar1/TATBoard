1721f594dae76977e7e0594b221d152869e78ae2
jobairalsarkar <jobairalsarkar1@gmail.com>
2024-01-31 22:21:36 +0600

    Modification to the Customer Model has been done. Email field is added, Dynamic Customer data is rendered to the front-end side.

--- app/models.py
+++ app/models.py
@@ -1,9 +1,10 @@
 from app import app, db
-from flask_login import UserMixin
+# from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
 
 
-class User(UserMixin, db.Model):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
@@ -12,6 +13,14 @@ class User(UserMixin, db.Model):
     profile_picture = db.Column(
         db.String(400), default='profile_pictures/default.jpg')
 
+    def __init__(self, name, email, password) -> None:
+        self.name = name
+        self.email = email
+        self.password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
+
     def __repr__(self) -> str:
         return f'{self.status}:{self.name}'
 
@@ -19,7 +28,8 @@ class User(UserMixin, db.Model):
 class Customer(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
-    # email = db.Column(db.String(50), unique=True, default='notusable@gmail.com',)
+    email = db.Column(db.String(50), unique=True,
+                      default='notusable@gmail.com')
     phone = db.Column(db.String(15), nullable=False)
     address = db.Column(db.String(300), nullable=False)
     amount = db.Column(db.Float(), nullable=False)
--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from app import app
 from flask import render_template
+from .models import User, Customer
 
 
 @app.route('/')
@@ -9,7 +10,8 @@ def home():
 
 @app.route('/customers')
 def customers():
-    return render_template('customers.html')
+    customers = Customer.query.all()
+    return render_template('customers.html', customers=customers)
 
 
 @app.route('/add_customers')
